28968dce4d3e4f4929d55610db10161995ff0f27
tushargupta26 <tushargupta.tg2609@gmail.com>
2024-09-21 16:59:59 +0530

    adding template behaviourial pattern

---
+++ src/behavioualPattern/templateDesignPattern/PayToFriend.java
@@ -0,0 +1,23 @@
+package behavioualPattern.templateDesignPattern;
+
+public class PayToFriend extends PaymentFlow{
+    @Override
+    public void validateRequest() {
+        System.out.println("Validating logic of payToFriend");
+    }
+
+    @Override
+    public void calculateFees() {
+        System.out.println("Calculating fees logic of payToFriend");
+    }
+
+    @Override
+    public void debitAmount() {
+        System.out.println("Debit the amount logic of payToFriend");
+    }
+
+    @Override
+    public void creditAmount() {
+        System.out.println("Credit the amount logic of payToFriend");
+    }
+}
---
+++ src/behavioualPattern/templateDesignPattern/PaymentFlow.java
@@ -0,0 +1,16 @@
+package behavioualPattern.templateDesignPattern;
+
+public abstract class PaymentFlow {
+
+    public abstract void validateRequest();
+    public abstract void calculateFees();
+    public abstract void debitAmount();
+    public abstract void creditAmount();
+
+    public final void sendMoney(){
+        validateRequest();
+        debitAmount();
+        calculateFees();
+        creditAmount();
+    }
+}
