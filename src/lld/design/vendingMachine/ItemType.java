e8e866dc7e4b37059d1f5461e946499d5f3dc898
tushargupta26 <tushargupta.tg2609@gmail.com>
2024-11-08 20:47:09 +0530

    Adding vending maching design

---
+++ src/lld/design/vendingMachine/ItemType.java
@@ -0,0 +1,10 @@
+package lld.design.vendingMachine;
+
+public enum ItemType {
+
+    COKE,
+    PEPSI,
+    JUICE,
+    SODA;
+}
+
---
+++ src/lld/design/vendingMachine/vendingStates/State.java
@@ -0,0 +1,26 @@
+package lld.design.vendingMachine.vendingStates;
+
+import lld.design.vendingMachine.Coin;
+import lld.design.vendingMachine.Item;
+import lld.design.vendingMachine.VendingMachine;
+
+import java.util.List;
+
+public interface State {
+
+    public void clickOnInsertCoinButton(VendingMachine machine) throws Exception;
+
+    public void clickOnStartProductSelectionButton(VendingMachine machine) throws Exception;
+
+    public void insertCoin(VendingMachine machine, Coin coin) throws Exception;
+
+    public void chooseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+    public int getChange(int returnChangeMoney) throws Exception;
+
+    public Item dispenseProduct(VendingMachine machine, int codeNumber) throws Exception;
+
+    public List<Coin> refundFullMoney(VendingMachine machine) throws Exception;
+
+    public void updateInventory(VendingMachine machine, Item item, int codeNumber) throws Exception;
+}
